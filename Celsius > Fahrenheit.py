6294d1c37116076c266e9f589e5153bd844c207b
Veer Mistry <67847413+Veer-M@users.noreply.github.com>
2023-06-22 19:44:20 -0700

    Update and rename 2.1.py to Celsius > Fahrenheit.py

--- Celsius > Fahrenheit.py
+++ Celsius > Fahrenheit.py
@@ -1,12 +1,8 @@
 '''
-   CIST 005A Summer 2023
-   HW week 1 problem 2.1
    Description: This program reads a Celsius degree and converts it to Fahrenheit
    Input: Console input of a Celsius Value
    Output: Console output of temperature value converted to Fahrenheit
-   Student: Veer Mistry
    Known bugs: Error message when enter a non-numerical value
-   Date: 6/21/2023
 '''
 
 Celsius = float(input("Enter a degree in Celsius: "))
