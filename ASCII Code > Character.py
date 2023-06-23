d61a3ee73ed6ddb29d768063b77e99e885e9fc22
Veer Mistry <67847413+Veer-M@users.noreply.github.com>
2023-06-22 19:46:27 -0700

    Update and rename 3.6.py to ASCII Code > Character.py

--- ASCII Code > Character.py
+++ ASCII Code > Character.py
@@ -1,12 +1,8 @@
 '''
-   CIST 005A Summer 2023
-   HW week 1 problem 3.6
    Description: This program receives an ASCII code and displays its character
    Input: Console input ASCII code
    Output: Console output ASCII character
-   Student: Veer Mistry
    Known bugs: Error if non-integer value or integer not between 0 and 127 is inputted
-   Date: 6/22/2023
 '''
 
 Numb = int(input("Enter an ASCII code: "))
