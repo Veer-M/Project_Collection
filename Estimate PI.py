9056a5f61d8b4d38974c0cfd5e25c952da592bbe
Veer Mistry <67847413+Veer-M@users.noreply.github.com>
2023-06-22 19:43:53 -0700

    Update and rename 1.7.py to Estimate PI.py

--- Estimate PI.py
+++ Estimate PI.py
@@ -1,12 +1,8 @@
 '''
-   CIST 005A Summer 2023
-   HW week 1 problem 1.7
    Description: Approximate pi using a formula
    Input: None
    Output: 2 estimated values of pi
-   Student: Veer Mistry
    Known bugs: None
-   Date: 6/20/2023
 '''
 
 print(4*(1-(1/3)+(1/5)-(1/7)+(1/9)-(1/11)))
