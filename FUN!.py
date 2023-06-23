93b1444f9ce0540ed1829ada054b0fba9f2045b3
Veer Mistry <67847413+Veer-M@users.noreply.github.com>
2023-06-22 19:43:18 -0700

    Update FUN!.py

--- FUN!.py
+++ FUN!.py
@@ -1,12 +1,8 @@
 '''
-   CIST 005A Summer 2023
-   HW week 1 problem 1.3
    Description: This program displays a pattern (FUN)
    Input: None
    Output: A pattern "FUN"
-   Student: Veer Mistry
    Known bugs: None
-   Date: 6/20/2023
 '''
 
 print("FFFFFFF  U     U  NN    NN")
